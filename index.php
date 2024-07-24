044bb424c7b1c35aaed1fdec800f9a3149437bf9
Ichchha Dhungel <ichchhadhungel@gmail.com>
2024-07-24 18:52:37 +0545

    Sorted buttons messages

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
     
     if(isset($_SESSION['access_token']))
     {
-        $btn_b_content = "Disconnect from Calendar";
+        $btn_a_content = "Connected to Calendar";
     }
     else
     {
-        $btn_b_content = "Connected to Calendar";
+        $btn_a_content = "Connect";
     }
 
 ?>
@@ -25,6 +25,14 @@
 
         <div class="row">
             <div class="col-4 link-container">
+                <form action="core/connect.php" class="btn-disconnect" method="post">
+                    <button type="submit" class="btn btn-primary"><?php echo $btn_a_content; ?></button>
+                </form>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-4 link-container">
                 <a href="views/list.php" class="btn btn-warning link-btn">View All Events</a>
                 <a href="views/create.php" class="btn btn-warning link-btn">Create New Events</a>
             </div>
@@ -33,7 +41,7 @@
         <div class="row">
             <div class="col-4 link-container">
                 <form action="core/disconnect.php" class="btn-disconnect" method="post">
-                    <button type="submit" class="btn btn-danger"><?php echo $btn_b_content; ?></button>
+                    <button type="submit" class="btn btn-danger">Disconnect</button>
                 </form>
             </div>
         </div>
